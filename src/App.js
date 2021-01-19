04f05a36fe31bc004131627b9f40ebe253ad0376
Gayle99 <gaylecassar@hotmail.com>
2021-01-19 15:30:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import React, {Component} from 'react';
+
+class App extends Component {
+  
+}
+
+export default App;
